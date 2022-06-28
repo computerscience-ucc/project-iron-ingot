860b71d249b8230cfb467e278558744880c2ee23
Jackdaisuki04@gmail.com <jackdaisuki04@gmail.com>
2022-06-28 18:40:51 +0800

    Post Author & Owner's Information
    Jack

--- schemas/capstone.js
+++ schemas/capstone.js
@@ -1,66 +1,92 @@
 export default {
-  type: 'document',
-  name: 'capstone',
-  title: 'Capstone',
+  type: "document",
+  name: "capstone",
+  title: "Capstone",
   fields: [
     {
-      type: 'image',
-      name: 'headerImage',
-      title: 'Capstone Image Header',
+      type: "image",
+      name: "headerImage",
+      title: "Capstone Image Header",
       options: {
         hotspot: true,
       },
     },
     {
-      title: 'Capstone Title',
-      name: 'capstoneTitle',
-      type: 'string',
+      title: "Capstone Title",
+      name: "capstoneTitle",
+      type: "string",
     },
     {
-      title: 'Slug',
-      name: 'slug',
-      type: 'slug',
+      title: "Slug",
+      name: "slug",
+      type: "slug",
       options: {
-        source: 'capstoneTitle',
+        source: "capstoneTitle",
         maxLength: 100,
       },
     },
     {
-      title: 'Capstone Author',
-      name: 'capstoneAuthor',
-      type: 'array',
+      title: "Post Author",
+      name: "postAuthor",
+      type: "array",
       of: [
         {
-          type: 'reference',
+          type: "reference",
           to: [
             {
-              type: 'author',
+              type: "author",
             },
           ],
         },
       ],
     },
     {
-      name: 'tags',
-      title: 'Tags',
-      type: 'array',
-      description: 'Enter searcheable keywords for the capstone',
+      type: 'object',
+      name: 'ownersInformation',
+      title: "Owner's Information",
+      options: {
+        collapsible: true,
+      },
+      fields: [
+    {
+      name: "ownerFirstname",
+      title: "Owner's Fullname",
+      type: "array",
+      description: "Enter Owners Name for this capstone project",
       of: [
         {
-          type: 'string',
+          type: "string",
         },
       ],
     },
     {
-      title: 'Capstone Content',
-      name: 'capstoneContent',
-      type: 'array',
+      title: "Owner's Section",
+      name: 'owner_section',
+      type: 'string',
+    },
+  ],
+},
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      description: "Enter searcheable keywords for the capstone",
       of: [
         {
-          type: 'block',
+          type: "string",
+        },
+      ],
+    },
+    {
+      title: "Capstone Content",
+      name: "capstoneContent",
+      type: "array",
+      of: [
+        {
+          type: "block",
         },
         {
-          type: 'image',
+          type: "image",
           options: {
             hotspot: true,
           },
