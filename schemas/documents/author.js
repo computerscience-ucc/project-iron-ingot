1a03dcb3d719d7a9c99019477e01cbb1d7127e80
Jackdaisuki04@gmail.com <jackdaisuki04@gmail.com>
2022-07-26 11:23:49 +0800

    updating capstone to thesis schema, add batch year level and year level, move full name to top in author schema

--- schemas/documents/author.js
+++ schemas/documents/author.js
@@ -1,64 +1,76 @@
 export default {
-  type: 'document',
-  name: 'author',
-  title: 'Author',
+  type: "document",
+  name: "author",
+  title: "Author",
   fields: [
     {
-      type: 'image',
-      name: 'authorPhoto',
-      title: 'Author Photo',
+      type: "object",
+      name: "fullName",
+      title: "Authors Full name",
+      options: {
+         collapsed: true,
+      },
+      fields: [
+        {
+          type: "string",
+          name: "firstName",
+          title: "First name",
+        },
+        {
+          type: "string",
+          name: "middleInitial",
+          title: "Middle initial",
+          validation: (Rule) => [
+            Rule.max(1).error("A middle initial of min. 1 character or none"),
+          ],
+        },
+        {
+          type: "string",
+          name: "lastName",
+          title: "Last name",
+        },
+      ],
+    },
+    {
+      type: "image",
+      name: "authorPhoto",
+      title: "Author Photo",
       options: {
         hotspot: true,
       },
     },
     {
-      type: 'string',
-      name: 'pronouns',
-      title: 'Pronouns',
-      description: 'Pronouns for the author',
+      type: "string",
+      name: "pronouns",
+      title: "Pronouns",
+      description: "Pronouns for the author",
     },
     {
-      title: 'Slug',
-      name: 'slug',
-      type: 'slug',
+      type: "string",
+      name: "batchYear",
+      title: "Batch Year",
+      description: "Batch Year of the Author",
+    },
+    {
+      type: "string",
+      name: "yearLevel",
+      title: "Year Level",
+      description: "Year Level of the Author",
+    },
+    {
+      title: "Slug",
+      name: "slug",
+      type: "slug",
       options: {
         source: (doc) => `${doc.fullName.lastName}-${doc.fullName.firstName}`,
         maxLength: 100,
       },
     },
-    {
-      type: 'object',
-      name: 'fullName',
-      title: 'Authors Full name',
-      options: {
-        collapsible: true,
-      },
-      fields: [
-        {
-          type: 'string',
-          name: 'firstName',
-          title: 'First name',
-        },
-        {
-          type: 'string',
-          name: 'middleInitial',
-          title: 'Middle initial',
-          validation: (Rule) => [
-            Rule.max(1).error('A middle initial of min. 1 character or none'),
-          ],
-        },
-        {
-          type: 'string',
-          name: 'lastName',
-          title: 'Last name',
-        },
-      ],
-    },
   ],
   preview: {
     select: {
-      title: 'fullName.lastName',
-      subtitle: 'author',
+      title: "fullName.lastName",
+      subtitle: "author",
     },
   },
 };
