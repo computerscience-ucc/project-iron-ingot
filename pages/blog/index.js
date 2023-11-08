a27ca3d2d4b18b00b9817281227146e2eaa2e888
computerscience-ucc <107482906+computerscience-ucc@users.noreply.github.com>
2023-11-08 21:17:54 +0800

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -39,7 +39,7 @@ const BlogPage = (e) => {
         <div className="flex flex-col gap-2 justify-center mt-16">
           <p className="text-4xl font-semibold">Blog</p>
           <p className="text-lg font-semibold">
-            See what CS students are up to in the CS department
+            See what CS students are up to in the CS Program
           </p>
         </div>
 
