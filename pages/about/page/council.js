607229794977f854c2d27fa6859936c12f4a14bb
Genrey <genreycristobal03@gmail.com>
2024-08-05 14:54:54 +0800

    Updated Council list

--- pages/about/page/council.js
+++ pages/about/page/council.js
@@ -22,114 +22,164 @@ const Page_Council = (e) => {
             </p>
           </div>
           <div className="col-span-full">
-            <p className="text-xl">Juan Dela Cruz</p>
+            <p className="text-xl">Jhude A. Vergara</p>
             <p className="text-sm text-yellow-600">President</p>
           </div>
           <div className="col-span-full">
-            <p className="text-xl">Mauirene R. Fuentes</p>
+            <p className="text-xl">Rosemarie A. Bullo</p>
             <p className="text-sm text-yellow-600">Vice-President</p>
           </div>
-          <div className="col-span-full">
+          <div className="col-span-full md:col-span-3">
             <p className="text-xl"> Jacelyn A. Caratao </p>
             <p className="text-sm text-yellow-600">Secretary</p>
           </div>
            <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Irheil Mae S. Antang</p>
+            <p className="text-xl">Heshekiah M. Tambago</p>
             <p className="text-sm text-yellow-600">Assistant Secretary</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Oscar A. Chin III</p>
+            <p className="text-xl">Karylle Dhoreign Daye E. de Torres</p>
             <p className="text-sm text-yellow-600">Treasurer</p>
           </div>
-          <div className="col-span-full md:col-span-3">
+          {/* <div className="col-span-full md:col-span-3">
             <p className="text-xl">Joshua Gabriel P. Dantes </p>
             <p className="text-sm text-yellow-600">Business Manager</p>
-          </div>
+          </div> */}
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Rosemarie A. Bullo</p>
+            <p className="text-xl">Krisha Mae P. Alcaide</p>
             <p className="text-sm text-yellow-600">Auditor</p>
           </div>
            <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Daniel D. Bobis</p>
-            <p className="text-sm text-yellow-600">Public Relations Officer</p>
-          </div>
-           <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Joshua Laurence T. Fabillon</p>
+            <p className="text-xl">Gero Earl S. Pereyra</p>
             <p className="text-sm text-yellow-600">Public Relations Officer</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Glenn M. Ugay</p>
+            <p className="text-xl">--</p>
+            <p className="text-sm text-yellow-600">Public Relations Officer</p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl">Jay Ann Rose B. Gerente</p>
             <p className="text-sm text-yellow-600">4th Year Representative</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Jhude A. Vergara</p>
+            <p className="text-xl">Ralph Michael B. Rocabo</p>
             <p className="text-sm text-yellow-600">3rd Year Representative</p>
           </div>
             <div className="col-span-full md:col-span-3">
-            <p className="text-xl">Krisha Mae P. Alcaide</p>
+            <p className="text-xl">Daniel D. Bobis</p>
             <p className="text-sm text-yellow-600">2nd Year Representative</p>
           </div>
             <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Maxine B. Valdez </p>
+            <p className="text-xl">--</p>
             <p className="text-sm text-yellow-600">1st Year Representative</p>
           </div>
-               <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Marianne Celest T. Jerez </p>
-            <p className="text-sm text-yellow-600">Creative Committee </p>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Gwyneth F. Uy </p>
+            <p className="text-sm text-yellow-600">Creative Committee Head </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Genrey O. Cristobal </p>
+            <p className="text-sm text-yellow-600">Program Committee Head </p>
           </div>
           <div className="col-span-full md:col-span-3">
             <p className="text-xl"> Stephanie Cueto </p>
             <p className="text-sm text-yellow-600">Creative Committee </p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Michael Angelo B. Baynosa </p>
-            <p className="text-sm text-yellow-600">Creative Committee </p>
-          </div>
-          <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Ma. Catherine H. Bae </p>
-            <p className="text-sm text-yellow-600">Program Committee </p>
-          </div>
-          <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Christian D. Perez </p>
+            <p className="text-xl"> Jhon Keneth Ryan B. Namias </p>
             <p className="text-sm text-yellow-600"> Program Committee</p>
           </div>
           <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Marainne Celest T. Jerez </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Gian Higino O. Fungo </p>
+            <p className="text-sm text-yellow-600">Program Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Mico Jake A. Lutiva </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Catherine Bae </p>
+            <p className="text-sm text-yellow-600">Program Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
             <p className="text-xl"> Mark Relan Gercee Acedo </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Irheil Mae Antang </p>
+            <p className="text-sm text-yellow-600">Program Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> -- </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Rafael Salve </p>
+            <p className="text-sm text-yellow-600">Program Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> -- </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> Denz Sabalbora </p>
+            <p className="text-sm text-yellow-600">Program Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> -- </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> -- </p>
+            <p className="text-sm text-yellow-600">Program Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> -- </p>
+            <p className="text-sm text-yellow-600">Creative Committee </p>
+          </div>
+          <div className="col-span-full md:col-span-3">
+            <p className="text-xl"> -- </p>
             <p className="text-sm text-yellow-600">Program Committee </p>
           </div>
         </div>
+
+
         <p className="text-2xl mt-24  font-semibold">Class Presidents</p>
         <div className="mt-8 grid grid-cols-1 md:grid-cols-6 gap-4">
             <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Mauirene Fuentes </p>
+            <p className="text-xl"> Rosemarie A. Bullo </p>
             <p className="text-sm text-yellow-600">BSCS 4A</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Glenn Ugay </p>
+            <p className="text-xl"> Jamaica Rose Grafil  </p>
             <p className="text-sm text-yellow-600">BSCS 4B</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Isaac Gabriel  Domino </p>
+            <p className="text-xl"> Joshua Gabriel P. Dantes </p>
             <p className="text-sm text-yellow-600">BSCS 3A</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Mark Rhicky Raby </p>
+            <p className="text-xl"> Laurence John Clarete </p>
             <p className="text-sm text-yellow-600">BSCS 3B</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Joshua Gabriel Dantes </p>
+            <p className="text-xl"> Sandra Agustin </p>
             <p className="text-sm text-yellow-600">BSCS 2A</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Laurence John Clarete </p>
+            <p className="text-xl"> Michael Baynosa </p>
             <p className="text-sm text-yellow-600">BSCS 2B</p>
           </div>
            <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Sandra Agustin </p>
+            <p className="text-xl"> -- </p>
             <p className="text-sm text-yellow-600">BSCS 1A</p>
           </div>
           <div className="col-span-full md:col-span-3">
-            <p className="text-xl"> Michael Angelo Baynosa </p>
+            <p className="text-xl"> -- </p>
             <p className="text-sm text-yellow-600">BSCS 1B </p>
           </div>
         </div>
