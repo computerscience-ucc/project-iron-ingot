9506ffb997ef202f07d22bd8dcb4b94e57e8e827
Jhon Keneth Namias <jkrbn99@gmail.com>
2026-06-19 18:56:58 +0800

    fix(studio): import presentationTool from sanity/presentation instead of @sanity/presentation

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
-import { presentationTool } from "@sanity/presentation";
+import { presentationTool } from "sanity/presentation";
 import { schemaTypes } from "./schemas/schema";
 import { deskStructure } from "./deskStructure";
 import { revalidateOnPublish } from "./documentActions";
@@ -18,9 +18,9 @@ const resolveProductionUrl = (doc) => {
     thesis: slug && `/thesis/${slug}`,
     award: slug && `/awards/${slug}`,
     gallery: slug && `/gallery/${slug}`,
-    council: `/council`,
-    devTeam: `/about`,
-    heroCarousel: `/`,
+    council: "/council",
+    devTeam: "/about",
+    heroCarousel: "/",
   };
 
   const path = routes[type];
